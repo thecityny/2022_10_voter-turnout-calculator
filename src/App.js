3ab2f8e299f508dcc2d24fea644a4600b43f6661
Sam Rabiyah <sraby@users.noreply.github.com>
2022-09-22 16:16:17 -0400

    Add first slider

--- src/App.js
+++ src/App.js
@@ -15,12 +15,16 @@ const App = () => {
   return (
     <div>
       <h1>Voter Turnout Prediction Calculator</h1>
-      <h2>Based on resutls from the 2018 gubernatorial election</h2>
+      <h2>Based on results from the 2018 gubernatorial election</h2>
       <Grid container spacing={2}>
         <Grid item xs={1}>
           <h2>IF:</h2>
         </Grid>
         <Grid item xs={3}>
+          <p>Biden's voters:</p>
+          <p>{100 - value[1]} for Hochul</p>
+          <p>{value[1] - value[0]} for Zeldin</p>
+          <p>{value[0]} don't vote</p>
       <Slider
         getAriaLabel={() => 'Temperature range'}
         orientation="vertical"
