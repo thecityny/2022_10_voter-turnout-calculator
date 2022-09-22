800ae53b10516be94434ddf3626c55eb52fec3c1
Sam Rabiyah <sraby@users.noreply.github.com>
2022-09-22 16:55:15 -0400

    add multi-color slider hack

--- src/App.js
+++ src/App.js
@@ -21,13 +21,14 @@ const App = () => {
           <h2>IF:</h2>
         </Grid>
         <Grid item xs={3}>
-          <p>Biden's voters:</p>
-          <p>{100 - value[1]} for Hochul</p>
-          <p>{value[1] - value[0]} for Zeldin</p>
-          <p>{value[0]} don't vote</p>
+          <p>Biden's voters are split:</p>
+          <p>{100 - value[1]}% for Hochul</p>
+          <p>{value[1] - value[0]}% for Zeldin</p>
+          <p>{value[0]}% don't vote</p>
       <Slider
-        getAriaLabel={() => 'Temperature range'}
+        data-size={value[0]}
         orientation="vertical"
+        className={`slider-position-${value[0]}`}
         sx={{
           height: 300,
           '& input[type="range"]': {
@@ -37,7 +38,9 @@ const App = () => {
         value={value}
         onChange={handleChange}
         valueLabelDisplay="auto"
-      />
+      >
+        <div className='boop'></div>
+      </Slider>
       </Grid>
       </Grid>
     </div>
