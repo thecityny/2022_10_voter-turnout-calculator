5a1a2197b91a7098642953bb720657c9544945b6
Sam Rabiyah <sraby@users.noreply.github.com>
2022-09-22 17:14:09 -0400

    Add second slider

--- src/App.js
+++ src/App.js
@@ -5,10 +5,23 @@ import Grid from "@mui/material/Grid";
 import "./styles/app.scss";
 
 const App = () => {
-  const [value, setValue] = useState([30, 50]);
+  /**
+   * This state holds the positions of the two break points on the slider widget
+   * for the democratic candidate.
+   */
+  const [demSliderPositions, setDemSliderPositions] = useState([30, 50]);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
+  /**
+   * This is a duplicate state but for the republican candidate.
+   */
+  const [repSliderPositions, setRepSliderPositions] = useState([30, 50]);
+
+  const handleDemChange = (event, newValue) => {
+    setDemSliderPositions(newValue);
+  };
+
+  const handleRepChange = (event, newValue) => {
+    setRepSliderPositions(newValue);
   };
 
   return (
@@ -21,23 +34,47 @@ const App = () => {
         </Grid>
         <Grid item xs={3}>
           <p>Biden's voters are split:</p>
-          <p className="color-dem">{100 - value[1]}% for Hochul</p>
-          <p className="color-rep">{value[1] - value[0]}% for Zeldin</p>
-          <p>{value[0]}% don't vote</p>
+          <p className="color-dem">{100 - demSliderPositions[1]}% for Hochul</p>
+          <p className="color-rep">
+            {demSliderPositions[1] - demSliderPositions[0]}% for Zeldin
+          </p>
+          <p>{demSliderPositions[0]}% don't vote</p>
           <Slider
-            data-size={value[0]}
+            data-size={demSliderPositions[0]}
             orientation="vertical"
-            className={`slider-position-${value[0]}`}
+            className={`dem-slider slider-position-${demSliderPositions[0]}`}
             sx={{
               height: 300,
               '& input[type="range"]': {
                 WebkitAppearance: "slider-vertical",
               },
             }}
-            value={value}
-            onChange={handleChange}
+            value={demSliderPositions}
+            onChange={handleDemChange}
             valueLabelDisplay="auto"
-          ></Slider>
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <p>Trump's voters are split:</p>
+          <p className="color-rep">{100 - repSliderPositions[1]}% for Hochul</p>
+          <p className="color-dem">
+            {repSliderPositions[1] - repSliderPositions[0]}% for Zeldin
+          </p>
+          <p>{repSliderPositions[0]}% don't vote</p>
+          <Slider
+            data-size={repSliderPositions[0]}
+            orientation="vertical"
+            className={`rep-slider slider-position-${repSliderPositions[0]}`}
+            sx={{
+              height: 300,
+              '& input[type="range"]': {
+                WebkitAppearance: "slider-vertical",
+              },
+            }}
+            value={repSliderPositions}
+            onChange={handleRepChange}
+            valueLabelDisplay="auto"
+          />
         </Grid>
       </Grid>
     </div>
