bfb19fbe3c76493673e9fed78ecb87072eee7916
Sam Rabiyah <sraby@users.noreply.github.com>
2022-09-27 16:46:13 -0400

    fix linting error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { VoterCalculator, VoterCalculatorSimple } from "./App";
+import { VoterCalculator } from "./App";
 
 ReactDOM.render(
   <Router>
